4c6b60478ebb75853d87a15983d23247d925544a
Ezequiel Santos <a21192@alunos.ipca.pt>
2024-12-10 17:44:16 +0000

    Backend Fornecedor - Login operacional

--- Backend/Fornecedor/ConnectionLoginF/routes/auth.js
+++ Backend/Fornecedor/ConnectionLoginF/routes/auth.js
@@ -7,11 +7,11 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     try {
         const pool = getPool();
-        const result = await pool.query('SELECT * FROM servicosBD.Credenciais WHERE email = $1', [email]);
+        const result = await pool.query('SELECT * FROM fornecedorBD.Credenciais_Fornecedores WHERE email = $1', [email]);
         const user = result.rows[0];
         if (!user || user.password !== password) return res.status(400).send('Invalid credentials');
 
-        const token = jwt.sign({ id: user.credenciaisid, isAdmin: user.utilizadorAdministrador }, 'secret', { expiresIn: '1h' });
+        const token = jwt.sign({ id: user.credenciaisid }, 'secret', { expiresIn: '1h' });
         res.json({ token });
     } catch (error) {
         res.status(400).send(error.message);
